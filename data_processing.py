a74ecc5bc938e0212fd2e39515703af6f5f1b3c1
Roy Ku <roynine90@gmail.com>
2023-11-06 01:20:13 +0900

    finished get_data()
    finished coding get data so that any item in the stock_list could be downloaded using yframe

--- data_processing.py
+++ data_processing.py
@@ -1,8 +1,41 @@
 from pandas_datareader import data as pdr
+import matplotlib.pyplot as plt
 import yfinance as yf
+import pandas as pd
+import numpy as np 
+import seaborn as sns
+from datetime import datetime
 
-def get_data():
+def get_data(stock_list, start, end):
     # Get data from Yahoo Finance
     yf.pdr_override()
-    data = pdr.get_data_yahoo("SPY", start="2017-01-01", end="2017-04-30")
-    return data
+
+    data = {}
+
+    for stock in stock_list:
+        data[stock] = yf.download(stock, start, end)
+    
+
+    company_list = [data['AAPL'], data['MSFT'], data['GOOG'], data['AMZN']]
+    company_name = ["APPLE", "GOOGLE", "MICROSOFT", "AMAZON"]
+
+    for company, com_name in zip(company_list, company_name):
+        company["company_name"] = com_name
+        
+    df = pd.concat(company_list, axis=0)
+    return df
+
+
+'''def create_graph_high():
+    plt.figure(figsize=(15, 10))
+    plt.subplots_adjust(top=1.25, bottom=1.2)
+
+    for i, company in enumerate(company_list, 1):
+        plt.subplot(2, 2, i)
+        company['High'].plot()
+        plt.ylabel('High Price')
+        plt.xlabel(None)
+        plt.title(f"High Price of {tech_list[i - 1]}")
+        
+    plt.tight_layout()
+'''
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from data_processing import get_data
+from datetime import datetime
 
-# Call the function
-data = get_data()
+def main():
+    stock_list = ['AAPL', 'MSFT', 'GOOG', 'AMZN']
+    end = datetime(2023, 10, 30)
+    start = datetime(2023, 9, 30)
 
-print(data)
+    data = get_data(stock_list, start, end)
+
+    print(data.head())
+    data.info() 
+
+if __name__ == '__main__':
+    main()
